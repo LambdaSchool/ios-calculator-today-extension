33c59ddddd367fdf756eb1c8fca59a57dbfc28b1
Linh Bouniol <paulalinh89@gmail.com>
2018-10-22 20:11:42 -0700

    final commit

--- Calculator/Calculator/CalculatorViewController.swift
+++ Calculator/Calculator/CalculatorViewController.swift
@@ -7,38 +7,41 @@ import UIKit
 import RPN
 
 class CalculatorViewController: UIViewController, UITextFieldDelegate {
+    
+    
+    private let numberFormatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.allowsFloats = true
+        formatter.maximumIntegerDigits = 20
+        formatter.minimumFractionDigits = 0
+        formatter.maximumFractionDigits = 20
+        return formatter
+    }()
+    
+    private var calculator = Calculator() {
+        didSet {
+            if let value = calculator.topValue {
+                textField.text = numberFormatter.string(from: value as NSNumber)
+            } else {
+                textField.text = ""
+            }
+        }
+    }
+    
+    // accumulate 1, 2, 3 into one number: 123
+    private var digitAccumulator = DigitAccumulator() {
+        didSet {
+            if let value = digitAccumulator.value() {
+                textField.text = numberFormatter.string(from: value as NSNumber)
+            } else {
+                textField.text = ""
+            }
+        }
+    }
+    
+    let groupUserDefaults = UserDefaults(suiteName: "group.linhbouniol.Calculator")!
 	
 	@IBOutlet var textField: UITextField!
-	
-	private let numberFormatter: NumberFormatter = {
-		let formatter = NumberFormatter()
-		formatter.allowsFloats = true
-		formatter.maximumIntegerDigits = 20
-		formatter.minimumFractionDigits = 0
-		formatter.maximumFractionDigits = 20
-		return formatter
-	}()
-	
-	private var calculator = Calculator() {
-		didSet {
-			if let value = calculator.topValue {
-				textField.text = numberFormatter.string(from: value as NSNumber)
-			} else {
-				textField.text = ""
-			}
-		}
-	}
-	
-    // accumulate 1, 2, 3 into one number: 123
-	private var digitAccumulator = DigitAccumulator() {
-		didSet {
-			if let value = digitAccumulator.value() {
-				textField.text = numberFormatter.string(from: value as NSNumber)
-			} else {
-				textField.text = ""
-			}
-		}
-	}
 	
 	@IBAction func numberButtonTapped(_ sender: UIButton) {
 		try? digitAccumulator.add(digit: .number(sender.tag))
@@ -74,10 +77,26 @@ class CalculatorViewController: UIViewController, UITextFieldDelegate {
 		returnButtonTapped(sender)
 		calculator.push(operator: .add)
 	}
-	
+    
+    @IBAction func changeThemeColor(_ sender: UIButton) {
+        view.tintColor = sender.tintColor
+        
+        if let colorData = try? NSKeyedArchiver.archivedData(withRootObject: sender.tintColor, requiringSecureCoding: true) {
+            groupUserDefaults.set(colorData, forKey: "ThemeColor")
+        }
+    }
+    
 	func textFieldShouldClear(_ textField: UITextField) -> Bool {
 		calculator.clear()
 		digitAccumulator.clear()
 		return true
 	}
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let colorData = groupUserDefaults.value(forKey: "ThemeColor") as? Data, let color = try? NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(colorData) as? UIColor {
+            view.tintColor = color
+        }
+    }
 }
