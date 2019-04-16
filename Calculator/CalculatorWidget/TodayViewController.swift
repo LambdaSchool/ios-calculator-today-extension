4c52ba49cea14e707fda7360e17fd28deebdd8ad
nateyoungren <nathanaelyoungren@gmail.com>
2019-04-16 15:08:19 -0700

    Add calculator functionality to widget.

--- Calculator/CalculatorWidget/TodayViewController.swift
+++ Calculator/CalculatorWidget/TodayViewController.swift
@@ -47,6 +47,70 @@ class TodayViewController: UIViewController, NCWidgetProviding {
         }
     }
     
+    @IBAction func numberButtonTapped(_ sender: UIButton) {
+        try? digitAccumulator.add(digit: .number(sender.tag))
+    }
+    
+    @IBAction func decimalButtonTapped(_ sender: UIButton) {
+        try? digitAccumulator.add(digit: .decimalPoint)
+    }
+    
+    @IBAction func returnButtonTapped(_ sender: UIButton) {
+        if let value = digitAccumulator.value() {
+            calculator.push(number: value)
+        }
+        digitAccumulator.clear()
+    }
+    
+    @IBAction func divideButtonTapped(_ sender: UIButton) {
+        returnButtonTapped(sender)
+        calculator.push(operator: .divide)
+    }
+    
+    @IBAction func multiplyButtonTapped(_ sender: UIButton) {
+        returnButtonTapped(sender)
+        calculator.push(operator: .multiply)
+    }
+    
+    @IBAction func subtractButtonTapped(_ sender: UIButton) {
+        returnButtonTapped(sender)
+        calculator.push(operator: .subtract)
+    }
+    
+    @IBAction func plusButtonTapped(_ sender: UIButton) {
+        returnButtonTapped(sender)
+        calculator.push(operator: .add)
+    }
+    
+    private var calculator = Calculator() {
+        didSet {
+            if let value = calculator.topValue {
+                calculatorTextField.text = numberFormatter.string(from: value as NSNumber)
+            } else {
+                calculatorTextField.text = ""
+            }
+        }
+    }
+    
+    private var digitAccumulator = DigitAccumulator() {
+        didSet {
+            if let value = digitAccumulator.value() {
+                calculatorTextField.text = numberFormatter.string(from: value as NSNumber)
+            } else {
+                calculatorTextField.text = ""
+            }
+        }
+    }
+    
+    private let numberFormatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.allowsFloats = true
+        formatter.maximumIntegerDigits = 20
+        formatter.minimumFractionDigits = 0
+        formatter.maximumFractionDigits = 20
+        return formatter
+    }()
+    
     @IBOutlet weak var rpnCalculatorLabel: UILabel!
     @IBOutlet weak var calculatorTextField: UITextField!
     @IBOutlet weak var rowOneStack: UIStackView!
